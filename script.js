ac1c92473828ad7ee48697256ce8a05622de7867
Lizzie Siegle <esiegle@brynmawr.edu>
2021-12-09 12:33:47 -0800

    twitch fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const {AIRTABLE_API_KEY, SIGNAL_SCAVENGER_HUNT_BASE_ID, TWILIO_STUDIO_SID, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER} = input.config();
+const {AIRTABLE_API_KEY, SIGNAL_SCAVENGER_HUNT_BASE_ID, TWILIO_STUDIO_SID, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER, recordsFoundMsgList, numRecordsFound, recordsFoundIds} = input.config();
 const studioCreateExecution = async (payload, twilioStudioSid, twilioAccountSid, twilioAuthToken) => {
     try {
         const basicAuth = b2a(`${twilioAccountSid}:${twilioAuthToken}`);
@@ -17,35 +17,7 @@ const studioCreateExecution = async (payload, twilioStudioSid, twilioAccountSid,
         throw e;
     }
 }
-const HYDRATE_TABLE_FIELDS = ['Message', 'Status'];
-const listRecords = async(apiKey, baseId, tableName, filterObject = {}, offsetId = null) => {
-    try {
-        let url = `https://api.airtable.com/v0/${baseId}/${tableName}?${serialize(filterObject)}`;
 
-        if(offsetId) {
-          url = `${url}&offset=${offsetId}`;
-        }
-
-        const response = await fetch(url, {
-          method: 'GET',
-          headers: {
-              'Authorization': `Bearer ${apiKey}`
-          }
-        });
-
-        const result = await response.json();
-        const {offset, records} = result;
-
-        if(offset) {
-          const additionalRecords = await listRecords(apiKey, baseId, tableName, filterObject, offset);
-          records.push(...additionalRecords);
-        }
-
-        return records;
-    } catch(e) {
-        return e;
-    }
-}
 
 //Credits: https://gist.github.com/oeon/0ada0457194ebf70ec2428900ba76255
 const b2a = (a) => {
@@ -56,18 +28,23 @@ const b2a = (a) => {
   return m = n.join(""), o = a.length % 3, (o ? m.slice(0, o - 3) :m) + "===".slice(o || 3);
 }
 let table = base.getTable('Lizzieairtablepost');
-let field = table.getField("Status");
+let field = table.getField("Sent");
 let query = await table.selectRecordsAsync();
+let recordMsgSentIdArr = [];
+let msg;
 const driver = async() => {
-    let filteredRecords = query.records.filter(record => {
-        let msg = record.getCellValue('Message');
-        let status = record.getCellValue("Status"); 
-        return msg !== null && status.name == "Todo";
-    });
-    let msg = filteredRecords.slice(-1); //last in array
-    
-    console.log(JSON.stringify(msg.toString().replace("name:", "")));
-    filteredRecords.forEach
+    console.log(`numRecordsFound ${numRecordsFound}`);
+    if(numRecordsFound == 0) {
+        for (let record of query.records) {
+            table.updateRecordAsync(record, {
+                "Sent": false,
+            })    
+        }
+        msg = "go get some water, you hardworking fiend"
+    }
+    else {
+        msg = recordsFoundMsgList.slice(-1); //first in array
+    }
     const payload = {
         'To': MY_PHONE_NUMBER,
         'From': TWILIO_PHONE_NUMBER,
@@ -75,7 +52,11 @@ const driver = async() => {
             'msg': msg
         })
     };
+    let recordMsgSentId = recordsFoundIds.slice(-1); //first in array
+    recordMsgSentIdArr.push(recordMsgSentId);
+    
     const result = await studioCreateExecution(payload, TWILIO_STUDIO_SID, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+    output.set('recordMsgSentId', recordMsgSentId);
     output.set('result', JSON.stringify(result));
 }
 await driver();
